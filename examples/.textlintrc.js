1813cb96c3f1419777183b692cabb49472d83fe5
ansanloms <16961622+ansanloms@users.noreply.github.com>
2026-08-31 19:20:34 +0900

    docs: examples/.textlintrc.js のコメントから package.json の記述を外す
    ラッパーディレクトリの package.json を削除したのに合わせ、コメントの
    参照先を index.js のみに改めた。

--- examples/.textlintrc.js
+++ examples/.textlintrc.js
@@ -1,4 +1,4 @@
-// preset-ansanloms は textlint-rule-preset-ansanloms/{package.json,index.js} を
+// preset-ansanloms は textlint-rule-preset-ansanloms/index.js を
 // 経由してロードされる。詳細は README の「使い方」を参照。
 module.exports = {
   rules: {
